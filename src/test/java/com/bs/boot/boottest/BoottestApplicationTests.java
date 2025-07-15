9de1803b0404a6febdcbeeb284ab9a7e61068159
MyeongSooJang <mmsgod0427@naver.com>
2025-07-15 09:53:26 +0900

    07-15 SpringBoot 시작 설정없이도 된다.

---
+++ src/test/java/com/bs/boot/boottest/BoottestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs.boot.boottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoottestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
